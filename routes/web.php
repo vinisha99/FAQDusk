3626ec12b4405b656b46e8670d9992e2280230b3
vinisha <vinisha99@gmail.com>
2018-11-18 19:04:04 -0500

    Task: Added Profile controller and updated existing pages to route to profile

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
