80b21d3dcf836db1e86b93ced84f027fbb8d404b
vinisha <vinisha99@gmail.com>
2018-11-22 03:21:09 -0500

    Task: Added View, edit, update and delete tasks for Answers

--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -10,8 +10,12 @@
                         {{$answer->body}}
                     </div>
                     <div class="card-footer">
+                        {{ Form::open(['method'  => 'DELETE', 'route' => ['answers.destroy', $question, $answer->id]])}}
+                        <button class="btn btn-danger float-right mr-2" value="submit" type="submit" id="submit">Delete
+                        </button>
+                        {!! Form::close() !!}
 
-                        <a class="btn btn-primary float-right" href="#">
+                        <a class="btn btn-primary float-right" href="{{ route('answers.edit',['question_id'=> $question, 'answer_id'=> $answer->id, ])}}">
                             Edit Answer
                         </a>
                     </div>
