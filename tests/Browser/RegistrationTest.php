379c7ac3d3caccce47918de0b672d3035144037a
vinisha <vinisha99@gmail.com>
2018-12-14 14:38:54 -0500

    Task: Registration page test case

---
+++ tests/Browser/RegistrationTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+
+class RegistrationTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /*
+
+    */
+    public function testRegistrationPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/register')
+                ->assertSee('Register');
+        });
+    }
+
+
+    /*
+
+     */
+  /*  public function testUserRegistration()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/register')
+                    ->type('email', 'abc@abc.com')
+                    ->type('password', 'secret')
+                    ->type('password_confirmation', 'secret')
+                    ->press('Register')
+                    ->assertPathIs('/home')
+                ;
+        });
+    }*/
+}
