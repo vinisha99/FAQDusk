82068fa7f2c5541898b8787475f6cdcdd4cb5a63
vinisha <vinisha99@gmail.com>
2018-12-14 16:49:47 -0500

    Fix: Login Page test

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\Browser\setup;
+use App\User;
+
+class LoginTest extends DuskTestCase
+{
+
+    use DatabaseMigrations;
+
+
+    public function testLoginPage()
+    {
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->assertSee('Login');
+        });
+    }
+
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testUserLogin()
+    {
+        parent::setUp();
+        $this->artisan('db:seed');
+
+        $newUser = $newUser = factory(User::class)->create([
+            'email' => 'someone@abc.com',
+            'password' => 'secret',
+
+        ]);
+
+        $this->browse(function (Browser $browser) {
+
+            $browser->loginAs(User::where(['email'=>'someone@abc.com'])->first())
+                ->visit('/home')
+                ->assertSee('Home');
+        });
+    }
+}
