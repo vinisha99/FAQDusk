1cbff510a5a3d735b36ad7b43a8c8563b5e0f28d
vinisha <vinisha99@gmail.com>
2018-12-15 16:59:36 -0500

    Task: Test to create, view, edit and delete questions

--- tests/Browser/QuestionTest.php
+++ tests/Browser/QuestionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Question;
 use App\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
@@ -16,6 +17,10 @@ class QuestionTest extends DuskTestCase
      *
      * @return void
      */
+
+    //public $questionID = 0;
+
+
     public function testQuestionPage()
     {
         $newUser = factory(User::class)->create([
@@ -42,10 +47,25 @@ class QuestionTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->loginAs(User::where(['email' => 'someone@abc.com'])->first())
                 ->visit('/home')
-                ->ClickLink('Create a Question')
+                ->clickLink('Create a Question')
                 ->assertPathIs('/questions/create');
         });
 
+    }
+
+
+    public function testNoQuestions()
+    {
+        $newUser = factory(User::class)->create([
+            'email' => 'someone@abc.com',
+            'password' => 'secret',
+        ]);
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::where(['email' => 'someone@abc.com'])->first())
+                ->visit('/home')
+                ->assertDontSee('View');
+        });
     }
 
 
@@ -62,10 +82,99 @@ class QuestionTest extends DuskTestCase
                 ->type('body', 'This is the First question')
                 ->press('Save')
                 ->assertPathIs('/home')
-                ->assertSee('IT WORKS!');
+                ->assertSee('IT WORKS!')
+                ->assertSee('View');
+        });
+    }
+
+
+    public function testViewQuestion()
+    {
+        $newUser = factory(User::class)->create([
+            'email' => 'someone@abc.com',
+            'password' => 'secret',
+        ]);
+
+        $newQuestion = factory(Question::class)->create([
+            'user_id' => User::where(['email' => 'someone@abc.com'])->first()->id,
+            'body' => 'This question is created for testing'
+        ]);
+
+        $newQuestion->save();
+        $questionID = '/questions/' . $newQuestion->id;
+
+        // dd($questionID);
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::where(['email' => 'someone@abc.com'])->first())
+                ->visit('/home')
+                ->clickLink('View')
+                ->assertPathIs('/questions/1')
+                ->assertSee('This question is created for testing');
+        });
+    }
+
+
+
+
+    public function testEditQuestion()
+    {
+        $newUser = factory(User::class)->create([
+            'email' => 'someone@abc.com',
+            'password' => 'secret',
+        ]);
+
+        $newQuestion = factory(Question::class)->create([
+            'user_id' => User::where(['email' => 'someone@abc.com'])->first()->id,
+            'body' => 'This question is created for testing'
+        ]);
+
+        $newQuestion->save();
+        /*$questionID = '/questions/'.$newQuestion->id;
+        $questionID->toString();*/
+
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::where(['email' => 'someone@abc.com'])->first())
+                ->visit('/questions/1')
+                ->clickLink('Edit Question')
+                ->type('body','The first question is edited')
+                ->press('Save')
+                ->assertSee('Saved');
+
         });
 
     }
+
+
+    public function testDeleteQuestion()
+    {
+        $newUser = factory(User::class)->create([
+            'email' => 'someone@abc.com',
+            'password' => 'secret',
+        ]);
+
+        $newQuestion = factory(Question::class)->create([
+            'user_id' => User::where(['email' => 'someone@abc.com'])->first()->id,
+            'body' => 'This question is created for testing'
+        ]);
+
+        $newQuestion->save();
+        /*$questionID = '/questions/'.$newQuestion->id;
+        $questionID->toString();*/
+
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::where(['email' => 'someone@abc.com'])->first())
+                ->visit('/questions/1')
+                ->press('Delete')
+                ->assertSee('Deleted');
+
+        });
+
+    }
+
+
 
 
 
